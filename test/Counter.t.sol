ed3c8585ebffd64009b17651d7165a51d2498fcb
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-01 18:33:47 +0800

    remap dependencies

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "@testing/test.sol";
 import "../src/Capsule.sol";
 
-contract CapsuleTest is Test {}
+contract CapsuleTest is DSTest {}
