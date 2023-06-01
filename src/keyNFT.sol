ed82359bb1f12097f915e057951049295ef9372e
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-01 21:24:56 +0800

    scaffold keyNFT

---
+++ src/keyNFT.sol
@@ -0,0 +1,65 @@
+/// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { ERC721 } from "@solmate/tokens/ERC721.sol";
+import { Strings } from "@openzeppelin/utils/Strings.sol";
+import { Ownable } from "@openzeppelin/access/Ownable.sol";
+
+error MintPriceNotPaid();
+error MaxSupply();
+error NonExistentTokenURI();
+error WithdrawTransfer();
+
+contract KeyNFT is ERC721, Ownable {
+
+    using Strings for uint256;
+    uint256 public currentTokenId;
+    string public baseURI;
+    uint256 public constant TOTAL_SUPPLY = 1_000;
+    uint256 public constant MINT_PRICE = 0.5 ether;
+    uint256 public constant MAX_PER_WALLET = 10;
+
+        constructor(
+        string memory _name,
+        string memory _symbol,
+        string memory _baseURI
+    ) ERC721(_name, _symbol) {
+        baseURI = _baseURI;
+    }
+
+    function mintTo(address recipient) public payable returns (uint256) {
+        if (msg.value != MINT_PRICE) {
+            revert MintPriceNotPaid();
+        }
+        uint256 newTokenId = ++currentTokenId;
+        if (newTokenId > TOTAL_SUPPLY) {
+            revert MaxSupply();
+        }
+        _safeMint(recipient, newTokenId);
+        return newTokenId;
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        virtual
+        override
+        returns (string memory)
+    {
+        if (ownerOf(tokenId) == address(0)) {
+            revert NonExistentTokenURI();
+        }
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString()))
+                : "";
+    }
+
+    function withdrawPayments(address payable payee) external onlyOwner {
+        uint256 balance = address(this).balance;
+        (bool transferTx, ) = payee.call{value: balance}("");
+        if (!transferTx) {
+            revert WithdrawTransfer();
+        }
+    }
+}
