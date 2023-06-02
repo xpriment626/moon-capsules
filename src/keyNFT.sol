e270dbb577adc2a3aaa5b53d4c85b7feed45e545
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-02 13:30:23 +0800

    simplify minting logic and handle URI storage

--- src/keyNFT.sol
+++ src/keyNFT.sol
@@ -2,63 +2,35 @@
 pragma solidity ^0.8.13;
 
 import { ERC721 } from "@openzeppelin/token/ERC721/ERC721.sol";
-import { Strings } from "@openzeppelin/utils/Strings.sol";
+import { ERC721URIStorage } from "@openzeppelin/token/ERC721/extensions/ERC721URIStorage.sol";
 import { Ownable } from "@openzeppelin/access/Ownable.sol";
 
-error MintPriceNotPaid();
-error MaxSupply();
-error NonExistentTokenURI();
-error WithdrawTransfer();
+contract KeyNFT is ERC721, ERC721URIStorage, Ownable {
 
-contract KeyNFT is ERC721, Ownable {
-
-    using Strings for uint256;
-    uint256 public currentTokenId;
-    string public baseURI;
-    uint256 public constant TOTAL_SUPPLY = 1_000;
-    uint256 public constant MINT_PRICE = 0.5 ether;
-
-    constructor(
-        string memory _name,
-        string memory _symbol,
-        string memory _baseURI
-    ) ERC721(_name, _symbol) {
-        baseURI = _baseURI;
+    constructor(address _recipient, uint256 _tokenId, string memory _uri) ERC721("MyNFT", "NFT") {
+        _safeMint(_recipient, _tokenId);
+        _setTokenURI(_tokenId, _uri);
     }
 
-    function mintTo(address recipient) public payable returns (uint256) {
-        if (msg.value != MINT_PRICE) {
-            revert MintPriceNotPaid();
-        }
-        uint256 newTokenId = ++currentTokenId;
-        if (newTokenId > TOTAL_SUPPLY) {
-            revert MaxSupply();
-        }
-        _safeMint(recipient, newTokenId);
-        return newTokenId;
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
     }
 
     function tokenURI(uint256 tokenId)
         public
         view
-        virtual
-        override
+        override(ERC721, ERC721URIStorage)
         returns (string memory)
     {
-        if (ownerOf(tokenId) == address(0)) {
-            revert NonExistentTokenURI();
-        }
-        return
-            bytes(baseURI).length > 0
-                ? string(abi.encodePacked(baseURI, tokenId.toString()))
-                : "";
+        return super.tokenURI(tokenId);
     }
 
-    function withdrawPayments(address payable payee) external onlyOwner {
-        uint256 balance = address(this).balance;
-        (bool transferTx, ) = payee.call{value: balance}("");
-        if (!transferTx) {
-            revert WithdrawTransfer();
-        }
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
     }
 }
