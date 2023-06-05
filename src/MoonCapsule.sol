8cc03375f242e526a4bb836aa859bcd8f5e720ac
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-05 16:07:18 +0800

    feat/capsule: handle ownership

--- src/MoonCapsule.sol
+++ src/MoonCapsule.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract MoonCapsule {}
+contract MoonCapsule {
+    address payable owner;
+
+    constructor(address payable _owner) {
+        owner = _owner;
+    }
+}
