73b1d112325d130d4b40c605831792600b7aaef6
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-05 17:21:35 +0800

    feat: change owner logic in Capsule contract

--- src/MoonCapsule.sol
+++ src/MoonCapsule.sol
@@ -1,10 +1,22 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-contract MoonCapsule {
-    address payable owner;
+import { IERC721 } from "@openzeppelin/interfaces/IERC721.sol";
 
-    constructor(address payable _owner) {
+contract MoonCapsule {
+
+    address payable public owner;
+    uint256 public associatedTokenId;
+    IERC721 public keyNFT;
+
+    constructor(address payable _owner, uint256 _tokenId, address _keyNFT)  {
         owner = _owner;
+        associatedTokenId = _tokenId;
+        keyNFT = IERC721(_keyNFT);
+    }
+
+    function updateOwner(address payable _newOwner) external {
+        require(_newOwner == keyNFT.ownerOf(associatedTokenId), "NFT owner does not match new owner");
+        owner = _newOwner;
     }
 }
