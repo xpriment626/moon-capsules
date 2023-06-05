635ece533daa21b6021041f9351c4deae9fdb0c4
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-05 16:06:14 +0800

    create factory contract

---
+++ src/MCFactory.sol
@@ -0,0 +1,10 @@
+// SPDC-License_Identifier: MIT
+pragma solidity ^0.8.13;
+
+import { MoonCapsule } from "./MoonCapsule.sol";
+import { KeyNFT } from "./KeyNFT.sol";
+
+contract MCFactory {
+    function createCapsule() external {}
+
+}
