ba290e526f42522b6ff6e2387141b7416b797617
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-05 17:18:17 +0800

    write test-specific logic into factory

--- src/MCFactory.sol
+++ src/MCFactory.sol
@@ -5,6 +5,13 @@ import { MoonCapsule } from "./MoonCapsule.sol";
 import { KeyNFT } from "./KeyNFT.sol";
 
 contract MCFactory {
-    function createCapsule() external {}
+
+    uint256 public tokenIdCounter = 1;
+    function createCapsule(address payable _receiver) external returns (KeyNFT, MoonCapsule){
+        KeyNFT key = new KeyNFT(_receiver, tokenIdCounter);
+        MoonCapsule capsule = new MoonCapsule(_receiver, tokenIdCounter, address(key));
+        tokenIdCounter++;
+        return (key, capsule);
+    }
 
 }
