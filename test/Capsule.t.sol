c62fb85ae5524da15c66c15323d6c35a9fe4ff22
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-12 20:39:08 +0800

    chore: redo tests with new logic

--- test/Capsule.t.sol
+++ test/Capsule.t.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.13;
 
 import "@testing/test.sol";
 import "../src/MoonCapsule.sol";
-import "../src/KeyNFT.sol";
 import "../src/MCFactory.sol";
 import {IERC721} from "@openzeppelin/interfaces/IERC721.sol";
 import {CheatCodes} from "./interfaces/CheatCodes.sol";
@@ -11,25 +10,66 @@ import {CheatCodes} from "./interfaces/CheatCodes.sol";
 contract CapsuleTest is DSTest {
     CheatCodes vm = CheatCodes(HEVM_ADDRESS);
 
-    event CapsuleCreated(address payable owner);
-    event checkAddr(address nftOwner, address capsuleOwner);
+    // event CapsuleCreated(address payable owner);
+    event CheckAddr(address nftOwner);
+    event EthReceived(address sender, uint256 amount);
 
     function testOwnerSanityTest() public {
-        address payable owner = payable(address(1));
         MCFactory factory = new MCFactory();
-        factory.createCapsule(owner);
-        MoonCapsule capsule = factory.capsules(owner);
-        IERC721 capsuleKey = capsule.keyNFT();
-        assertEq(capsuleKey.ownerOf(1), capsule.owner());
+        address payable alice = payable(address(1));
+        vm.prank(alice);
+        factory.createCapsule(alice);
+        MoonCapsule capsule = factory.capsules(alice);
+        address owner = capsule.ownerOf(1);
+        assertEq(owner, alice);
     }
 
-    function testCapsuleCreatedEmit() public {
-        address payable owner = payable(
-            0xA66D38D132461f69b5aA1958233Ee120f513D451
-        );
+    function testMultiOwner() public {
         MCFactory factory = new MCFactory();
-        vm.expectEmit(true, false, false, false);
-        emit CapsuleCreated(owner);
-        factory.createCapsule(owner);
+
+        /// Alice creates a capsule
+        address payable alice = payable(address(1));
+        address payable bob = payable(address(2));
+        vm.prank(alice);
+        factory.createCapsule(alice);
+        MoonCapsule capsuleAlice = factory.capsules(alice);
+        address owner = capsuleAlice.ownerOf(1);
+
+        /// Bob creates a capsule
+        vm.prank(bob);
+        factory.createCapsule(bob);
+        MoonCapsule capsuleBob = factory.capsules(bob);
+        address owner2 = capsuleBob.ownerOf(2);
+        assertEq(owner, alice);
+        assertEq(owner2, bob);
+    }
+
+    function testMultiOwnerBalances() public {
+        MCFactory factory = new MCFactory();
+
+        /// Alice funds a capsule
+        address payable alice = payable(address(1));
+        address payable bob = payable(address(2));
+        vm.prank(alice);
+        factory.createCapsule(alice);
+        MoonCapsule capsuleAlice = factory.capsules(alice);
+        vm.deal(alice, 10 ether);
+        vm.prank(alice);
+        payable(address(capsuleAlice)).transfer(10 ether);
+
+        /// Bob funds a capsule
+        vm.prank(bob);
+        factory.createCapsule(bob);
+        MoonCapsule capsuleBob = factory.capsules(bob);
+        vm.deal(bob, 10 ether);
+        vm.prank(bob);
+        payable(address(capsuleBob)).transfer(10 ether);
+
+        /// Check balances
+        uint256 aliceBalance = address(capsuleAlice).balance;
+        uint256 bobBalance = address(capsuleBob).balance;
+
+        assertEq(aliceBalance, 10 ether);
+        assertEq(bobBalance, 10 ether);
     }
 }
