0f0521a6cb9ce0fb632672bedc19844647a3bf41
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-12 20:38:18 +0800

    refactor: mooncapsule is ERC721

--- src/MoonCapsule.sol
+++ src/MoonCapsule.sol
@@ -1,27 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import {IERC721} from "@openzeppelin/interfaces/IERC721.sol";
+import {ERC721} from "@openzeppelin/token/ERC721/ERC721.sol";
 
-error NonMatchingKey(address expected);
+contract MoonCapsule is ERC721 {
+    event EthReceived(address sender, uint256 amount);
 
-contract MoonCapsule {
-    address payable public owner;
-    uint256 public associatedTokenId;
-    IERC721 public keyNFT;
-
-    constructor(address payable _owner, uint256 _tokenId, address _keyNFT) {
-        owner = _owner;
-        associatedTokenId = _tokenId;
-        keyNFT = IERC721(_keyNFT);
+    constructor(
+        address _receiver,
+        uint256 _factoryId
+    ) ERC721("MoonCapsule", "iMCT") {
+        _safeMint(_receiver, _factoryId);
     }
 
-    function updateOwner(address payable _newOwner) external {
-        if (_newOwner == keyNFT.ownerOf(associatedTokenId)) {
-            revert NonMatchingKey({
-                expected: keyNFT.ownerOf(associatedTokenId)
-            });
-        }
-        owner = _newOwner;
+    receive() external payable {
+        emit EthReceived(msg.sender, msg.value);
     }
 }
