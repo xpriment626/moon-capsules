383a84992802f67705b7cab738626636aa103d5b
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-12 18:54:57 +0800

    refactor: use custom error

--- src/MoonCapsule.sol
+++ src/MoonCapsule.sol
@@ -1,22 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { IERC721 } from "@openzeppelin/interfaces/IERC721.sol";
+import {IERC721} from "@openzeppelin/interfaces/IERC721.sol";
+
+error NonMatchingKey(address expected);
 
 contract MoonCapsule {
-
     address payable public owner;
     uint256 public associatedTokenId;
     IERC721 public keyNFT;
 
-    constructor(address payable _owner, uint256 _tokenId, address _keyNFT)  {
+    constructor(address payable _owner, uint256 _tokenId, address _keyNFT) {
         owner = _owner;
         associatedTokenId = _tokenId;
         keyNFT = IERC721(_keyNFT);
     }
 
     function updateOwner(address payable _newOwner) external {
-        require(_newOwner == keyNFT.ownerOf(associatedTokenId), "NFT owner does not match new owner");
+        if (_newOwner == keyNFT.ownerOf(associatedTokenId)) {
+            revert NonMatchingKey({
+                expected: keyNFT.ownerOf(associatedTokenId)
+            });
+        }
         owner = _newOwner;
     }
 }
