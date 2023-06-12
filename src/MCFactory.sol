10a56c278882d90519e9500fe9e7bcd1c0370f47
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-12 13:23:43 +0800

    feat: CapsuleCreated event

--- src/MCFactory.sol
+++ src/MCFactory.sol
@@ -1,17 +1,35 @@
-// SPDC-License_Identifier: MIT
+// SPDX-License_Identifier: MIT
 pragma solidity ^0.8.13;
 
-import { MoonCapsule } from "./MoonCapsule.sol";
-import { KeyNFT } from "./KeyNFT.sol";
+import {MoonCapsule} from "./MoonCapsule.sol";
+import {KeyNFT} from "./KeyNFT.sol";
 
 contract MCFactory {
+    event CapsuleCreated(
+        address capsuleAddress,
+        address keyAddress,
+        address receiver,
+        uint256 tokenId
+    );
 
     uint256 public tokenIdCounter = 1;
-    function createCapsule(address payable _receiver) external returns (KeyNFT, MoonCapsule){
+
+    function createCapsule(
+        address payable _receiver
+    ) external returns (KeyNFT, MoonCapsule) {
         KeyNFT key = new KeyNFT(_receiver, tokenIdCounter);
-        MoonCapsule capsule = new MoonCapsule(_receiver, tokenIdCounter, address(key));
+        MoonCapsule capsule = new MoonCapsule(
+            _receiver,
+            tokenIdCounter,
+            address(key)
+        );
+        emit CapsuleCreated(
+            address(capsule),
+            address(key),
+            _receiver,
+            tokenIdCounter
+        );
         tokenIdCounter++;
         return (key, capsule);
     }
-
 }
