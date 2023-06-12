c7398329f8f590c8fc0340776b1b20c78cbd9970
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-12 14:34:46 +0800

    refactor: CapsuleCreated event

--- src/MCFactory.sol
+++ src/MCFactory.sol
@@ -5,31 +5,20 @@ import {MoonCapsule} from "./MoonCapsule.sol";
 import {KeyNFT} from "./KeyNFT.sol";
 
 contract MCFactory {
-    event CapsuleCreated(
-        address capsuleAddress,
-        address keyAddress,
-        address receiver,
-        uint256 tokenId
-    );
+    event CapsuleCreated(address payable owner);
 
     uint256 public tokenIdCounter = 1;
+    mapping(address => MoonCapsule) public capsules;
 
-    function createCapsule(
-        address payable _receiver
-    ) external returns (KeyNFT, MoonCapsule) {
+    function createCapsule(address payable _receiver) external {
         KeyNFT key = new KeyNFT(_receiver, tokenIdCounter);
         MoonCapsule capsule = new MoonCapsule(
             _receiver,
             tokenIdCounter,
             address(key)
         );
-        emit CapsuleCreated(
-            address(capsule),
-            address(key),
-            _receiver,
-            tokenIdCounter
-        );
+        capsules[_receiver] = capsule;
         tokenIdCounter++;
-        return (key, capsule);
+        emit CapsuleCreated(_receiver);
     }
 }
