f7b3ece9d57c5b2d3718ce9ed765054faf53dc7e
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-13 16:47:47 +0800

    refactor: mock erc20 owner logic

--- test/mocks/MockERC20.sol
+++ test/mocks/MockERC20.sol
@@ -2,11 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {ERC20} from "@openzeppelin/token/ERC20/ERC20.sol";
+import {Ownable} from "@openzeppelin/access/Ownable.sol";
 
-contract MockERC20 is ERC20 {
+contract MockERC20 is ERC20, Ownable {
     constructor() ERC20("MyToken", "MTK") {}
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 }
