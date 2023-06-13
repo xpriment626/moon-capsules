823e01c0cbc711a67a9f9acca20f658f76d4833f
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-13 15:05:18 +0800

    feat: eth withdrawal

--- src/MoonCapsule.sol
+++ src/MoonCapsule.sol
@@ -2,10 +2,12 @@
 pragma solidity ^0.8.13;
 
 import {ERC721} from "@openzeppelin/token/ERC721/ERC721.sol";
+import {IERC20} from "@openzeppelin/interfaces/IERC20.sol";
+
+error NotOwner(address caller, address expected);
+error InsufficientBalance(uint256 balance, uint256 amount);
 
 contract MoonCapsule is ERC721 {
-    event EthReceived(address sender, uint256 amount);
-
     constructor(
         address _receiver,
         uint256 _factoryId
@@ -13,7 +15,19 @@ contract MoonCapsule is ERC721 {
         _safeMint(_receiver, _factoryId);
     }
 
-    receive() external payable {
-        emit EthReceived(msg.sender, msg.value);
+    function withdraw(uint256 _amount, uint256 _tokenId) external payable {
+        if (msg.sender != ownerOf(_tokenId)) {
+            revert NotOwner({caller: msg.sender, expected: ownerOf(_tokenId)});
+        }
+        if (address(this).balance < _amount) {
+            revert InsufficientBalance({
+                balance: address(this).balance,
+                amount: _amount
+            });
+        }
+        (bool sent, ) = payable(msg.sender).call{value: _amount}("");
+        require(sent, "Withdrawal failed");
     }
+
+    receive() external payable {}
 }
