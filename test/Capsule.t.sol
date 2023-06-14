6d001934e8abbf253bb15235d2889834ec7eaf3a
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-14 14:13:08 +0800

    chore: test erc20 withdrawal after owner swap

--- test/Capsule.t.sol
+++ test/Capsule.t.sol
@@ -158,21 +158,28 @@ contract CapsuleTest is DSTest {
         token.approve(alice, 1000);
         assertEq((token.allowance(alice, alice)), 1000);
         token.transferFrom(alice, address(capsule), 1000);
+        assertEq(token.allowance(alice, alice), 0);
         vm.stopPrank();
         assertEq(token.balanceOf(address(capsule)), 1000);
         assertEq(token.balanceOf(alice), 0);
 
         /// Alice transfer back to herself
-        vm.prank(alice);
+        vm.startPrank(alice);
         capsule.withdrawERC20(address(token), 100, 1);
+        token.approve(alice, token.balanceOf(alice));
+        vm.stopPrank();
         assertEq(token.balanceOf(alice), 100);
+        assertEq(token.allowance(alice, alice), 100);
 
-        vm.prank(alice);
-        capsule.withdrawERC20(address(token), 500, 1);
-        assertEq(token.balanceOf(alice), 600);
+        vm.startPrank(alice);
+        capsule.withdrawERC20(address(token), 600, 1);
+        token.approve(alice, token.balanceOf(alice));
+        vm.stopPrank();
+        assertEq(token.balanceOf(alice), 700);
+        assertEq(token.allowance(alice, alice), 700);
     }
 
-    function testSwapOwnership() public {
+    function testSwapableOwnership() public {
         MCFactory factory = new MCFactory();
         address payable alice = payable(address(1));
         address payable bob = payable(address(2));
@@ -197,5 +204,51 @@ contract CapsuleTest is DSTest {
         capsule.withdraw(5 ether, 1);
         assertEq(address(capsule).balance, 5 ether);
         assertEq(address(bob).balance, 5 ether);
+    }
+
+    function testSwapableOwnershipERC20() public {
+        MCFactory factory = new MCFactory();
+        address payable alice = payable(address(1));
+        address payable bob = payable(address(2));
+
+        /// Mint token and send to Alice
+        MockERC20 token = new MockERC20();
+        token.mint(address(this), 1000);
+        token.approve(address(this), 1000);
+        assertEq((token.allowance(address(this), address(this))), 1000);
+        token.transferFrom(address(this), alice, 1000);
+        assertEq(token.balanceOf(alice), 1000);
+
+        /// Alice creates capsule and deposits ERC20 tokens
+        vm.startPrank(alice);
+        factory.createCapsule(alice);
+        MoonCapsule capsule = factory.capsules(alice);
+        token.approve(alice, 1000);
+        assertEq((token.allowance(alice, alice)), 1000);
+        token.transferFrom(alice, address(capsule), 1000);
+        assertEq(token.allowance(alice, alice), 0);
+        vm.stopPrank();
+        assertEq(token.balanceOf(address(capsule)), 1000);
+        assertEq(token.balanceOf(alice), 0);
+
+        /// Alice transfers capsule to Bob
+        vm.prank(alice);
+        capsule.transferFrom(alice, bob, 1);
+        assertEq(capsule.ownerOf(1), bob);
+
+        /// Bob withdraws tokens deposited by Alice
+        vm.startPrank(bob);
+        capsule.withdrawERC20(address(token), 100, 1);
+        token.approve(bob, token.balanceOf(bob));
+        vm.stopPrank();
+        assertEq(token.balanceOf(bob), 100);
+        assertEq(token.allowance(bob, bob), 100);
+
+        vm.startPrank(bob);
+        capsule.withdrawERC20(address(token), 600, 1);
+        token.approve(bob, token.balanceOf(bob));
+        vm.stopPrank();
+        assertEq(token.balanceOf(bob), 700);
+        assertEq(token.allowance(bob, bob), 700);
     }
 }
