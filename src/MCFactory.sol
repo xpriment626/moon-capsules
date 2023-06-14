f8a7b525f14a6688050ac2043501956942bd609c
Emmett Childs <emmettchilds@thegobospot.com>
2023-06-14 22:25:23 +0800

    refactor: add fields to CapsuleCreated event

--- src/MCFactory.sol
+++ src/MCFactory.sol
@@ -4,14 +4,14 @@ pragma solidity ^0.8.13;
 import "./MoonCapsule.sol";
 
 contract MCFactory {
-    event CapsuleCreated(address payable owner);
+    event CapsuleCreated(address _owner, uint256 _tokenId, address capsule);
     uint256 public tokenId = 1;
     mapping(address => MoonCapsule) public capsules;
 
     function createCapsule(address _receiver) public {
         MoonCapsule capsule = new MoonCapsule(_receiver, tokenId);
         capsules[_receiver] = capsule;
+        emit CapsuleCreated(_receiver, tokenId, address(capsule));
         tokenId++;
-        emit CapsuleCreated(payable(address(capsule)));
     }
 }
